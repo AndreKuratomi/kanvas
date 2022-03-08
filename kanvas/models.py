15c549e34dd6aad63d27588f7f02486cac0494f0
André Kuratomi <andrekuratomi@gmail.com>
2022-03-08 17:09:06 -0300

    feat: Address model drafted and second migration made

--- kanvas/models.py
+++ kanvas/models.py
@@ -1,3 +1,4 @@
+from types import CoroutineType
 from django.db import models
 import uuid
 
@@ -9,4 +10,12 @@ class Course(models.Model):
     created_at: models.DateTimeField()
     link_repo: models.CharField(max_length=255)
 
-# Create your models here.
+
+class Address(models.Model):
+    uuid: models.UUIDField(default=uuid.uuid4, editable=False)
+    city: models.CharField(max_length=255)
+    country: models.CharField(max_length=255)
+    state: models.CharField(max_length=255)
+    street: models.CharField(max_length=255)
+    house_number: models.IntegerField()
+    zip_code: models.CharField(max_length=255, unique=False)
