41377006465d730e3afd693801ceb39f9fdf230f
André Kuratomi <andrekuratomi@gmail.com>
2022-03-09 20:44:20 -0300

    refactor: 'max_length' removed from serializers

--- kanvas/serializers.py
+++ kanvas/serializers.py
@@ -5,22 +5,22 @@ import uuid
 class AddressSerializer(serializers.Model):
     uuid = serializers.UUIDField(default=uuid.uuid4, editable=False)
 
-    city = serializers.CharField(max_length=255)
-    country = serializers.CharField(max_length=255)
-    state = serializers.CharField(max_length=255)
-    street = serializers.CharField(max_length=255)
+    city = serializers.CharField()
+    country = serializers.CharField()
+    state = serializers.CharField()
+    street = serializers.CharField()
     house_number = serializers.IntegerField()
-    zip_code = serializers.CharField(max_length=255, unique=False)
+    zip_code = serializers.CharField(unique=False)
 
 
 class UserSerializer(serializers.Serializer):
     uuid = serializers.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
 
     is_admin = serializers.BooleanField()
-    email = serializers.CharField(max_length=255, unique=True)
-    password = serializers.CharField(max_length=255)
-    first_name = serializers.CharField(max_length=255)
-    last_name = serializers.CharField(max_length=255)
+    email = serializers.CharField(unique=True)
+    password = serializers.CharField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
 
     address = AddressSerializer(many=True, read_only=True)
 
@@ -29,9 +29,9 @@ class CourseSerializer(serializers.Serializer):
     uuid = serializers.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
     created_at = serializers.DateTimeField()
 
-    name = serializers.CharField(max_length=255, unique=True)
+    name = serializers.CharField(unique=True)
     demo_time = serializers.TimeField()
-    link_repo = serializers.CharField(max_length=255)
+    link_repo = serializers.CharField()
 
     # if user.is_admin == True:
     user_instructor = UserSerializer(read_only=True)
